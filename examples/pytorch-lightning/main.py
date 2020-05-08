43cb592208cb9b4b1eaba9f54a3af7af14325b5d
Thomas Chaton <thomas.chaton.ai@gmail.com>
2020-05-08 20:18:54 +0100

    still some bugs with kpconv

--- examples/pytorch-lightning/main.py
+++ examples/pytorch-lightning/main.py
@@ -1,45 +1,43 @@
-try:
-    # Used for dev
-    from utils import load_torch_points3d
-    load_torch_points3d()
-except:
-    pass
-
+from utils import load_torch_points3d
+load_torch_points3d()
 import os
+import sys
 import torch
 from torch import nn
 from omegaconf import OmegaConf
 from torch.nn import functional as F
+from torch_points3d.applications.pointnet2 import PointNet2
+from torch_points3d.applications.kpconv import KPConv
+from torch_points3d.applications.rsconv import RSConv
 from torch_points3d.datasets.classification.modelnet import ModelNetDataset
 from torch_points3d.core.common_modules.base_modules import Seq
 from torch_points3d.core.common_modules.dense_modules import Conv1D
-from torch_points3d.applications.pointnet2 import PointNet2
 import pytorch_lightning as pl
-from pytorch_lightning import Trainer
-
 
 class Model(pl.LightningModule):
 
     def __init__(self, params):
         super().__init__()
-        
-        self._model_opt = params.model
-        self._dataset_opt = params.data
-        self._training_opt = params.training
-        self._input_nc = eval(self._model_opt.input_nc)
-        self._output_nc = eval(self._model_opt.output_nc)
-        self._model = nn.ModuleDict()
-        self._model["encoder"] = PointNet2(input_nc=self._input_nc, 
-                                  output_nc=self._output_nc,
-                                  in_feat=self._model_opt.in_feat, 
-                                  multiscale=self._model_opt.multiscale, 
-                                  num_layers=self._model_opt.num_layers, architecture="encoder")
-        
+        self._params = params
+        self._build_backbone()
         self._model["classifier"] = Seq()
-        self._model["classifier"].append(Conv1D(self._output_nc, self._dataset_opt.number, activation=None, bias=True, bn=False))
+        self._model["classifier"].append(Conv1D(self._model_opt.output_nc, self._params.data.number, activation=None, bias=True, bn=False))
+
+    def _build_backbone(self):
+        self._model = nn.ModuleDict()
+        model_name = sys.argv[1] if len(sys.argv) == 2 else "pointnet2"
+        assert model_name in ["pointnet2", "pointnet", "kpconv", "rsconv"], "model_name should within ['pointnet2', 'pointnet', 'kpconv', 'rsconv']"
+        self._model_opt = getattr(self._params.models, model_name)
+        model_builder = globals().copy()[self._model_opt.class_name]
+        self._model["backbone"] = model_builder(architecture="encoder", 
+                                                input_nc=self._model_opt.input_nc, 
+                                                in_feat=self._model_opt.in_feat,
+                                                num_layers=self._model_opt.num_layers,
+                                                output_nc=self._model_opt.output_nc,
+                                                multiscale=True)
 
     def forward(self, data):
-        data_out = self._model["encoder"](data)
+        data_out = self._model["backbone"](data)
         return F.log_softmax(self._model["classifier"](data_out.x).squeeze(), dim=-1)
 
     def training_step(self, data, *args):
@@ -64,14 +62,8 @@ class Model(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=0.001)
 
     def prepare_data(self):
-        self._dataset = ModelNetDataset(self._dataset_opt)
-        self._dataset.create_dataloaders(
-            self._model_opt,
-            self._training_opt.batch_size,
-            True,
-            self._training_opt.num_workers,
-            False,
-        )
+        self._dataset = ModelNetDataset(self._params.data)
+        self._dataset.create_dataloaders(self._model_opt, self._params.training.batch_size, True, self._params.training.num_workers, False)
 
     def train_dataloader(self):
         return self._dataset._train_loader
@@ -82,31 +74,9 @@ class Model(pl.LightningModule):
 def main(params):
 
     model = Model(params=params)
-
-    # most basic trainer, uses good defaults
-    trainer = Trainer(gpus=0, progress_bar_refresh_rate=10, max_epochs=10)
+    trainer = pl.Trainer(gpus=0, progress_bar_refresh_rate=10, max_epochs=10)
     trainer.fit(model)   
 
 if __name__ == '__main__':
-
-    params = """
-    data:
-        dataroot: data
-        num_points: 1024
-        number: 10
-        use_normal: False
-    model:
-        conv_type: "dense"
-        input_nc: 3 * (1 * ${data.use_normal})
-        in_feat: 16
-        output_nc: 16 * ${model.in_feat}
-        num_layers: 3
-        multiscale: True
-    training:
-        batch_size: 4
-        num_workers: 1
-    """
-
-    params = OmegaConf.create(params)
-
+    params = OmegaConf.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), "main.yaml"))
     main(params)
--- examples/pytorch-lightning/utils.py
+++ examples/pytorch-lightning/utils.py
@@ -1,15 +1,19 @@
-def load_torch_points3d():
-    import os
-    import sys
-    from importlib import util
-    DIR = os.path.dirname(os.path.realpath(__file__))
-    torch_points3d_path = os.path.join(
-        DIR, "..", "..", "torch-points3d", "torch_points3d")
-    assert os.path.exists(torch_points3d_path)
+import os
+import sys
+from importlib import util
 
-    MODULE_PATH = os.path.join(torch_points3d_path, "__init__.py")
-    MODULE_NAME = "torch_points3d"
-    spec = util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
-    module = util.module_from_spec(spec)
-    sys.modules[spec.name] = module
-    spec.loader.exec_module(module)
+def load_torch_points3d():
+    try:
+        DIR = os.path.dirname(os.path.realpath(__file__))
+        torch_points3d_path = os.path.join(
+            DIR, "..", "..", "torch-points3d", "torch_points3d")
+        assert os.path.exists(torch_points3d_path)
+
+        MODULE_PATH = os.path.join(torch_points3d_path, "__init__.py")
+        MODULE_NAME = "torch_points3d"
+        spec = util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
+        module = util.module_from_spec(spec)
+        sys.modules[spec.name] = module
+        spec.loader.exec_module(module)
+    except:
+        pass
